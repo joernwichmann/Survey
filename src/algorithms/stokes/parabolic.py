b2184192cf78cfc7fd7b9888de65fbb4db3a2037
Joern Wichmann <joern.wichmann@monash.edu>
2025-03-04 10:56:41 +1100

    implemented modified Chorin splitting

--- src/algorithms/stokes/parabolic.py
+++ src/algorithms/stokes/parabolic.py
@@ -18,6 +18,8 @@ def get_algorithm_by_name(algorithm_name: str) -> StokesAlgorithm:
     match algorithm_name:
         case "Chorin splitting":
             return Chorin_splitting
+        case "modified Chorin splitting":
+            return Chorin_splitting_with_pressure_correction
         case "Implicit Euler mixed FEM":
             return implicitEuler_mixedFEM
         case "Crank Nicolson mixed FEM Stratonovich Transport Noise":
@@ -181,6 +183,116 @@ def Chorin_splitting(space_disc: SpaceDiscretisation,
         uold.assign(unew)
 
     return time_to_velocity, time_to_pressure
+
+def Chorin_splitting_with_pressure_correction(space_disc: SpaceDiscretisation,
+                     time_grid: list[float],
+                     noise_coeff_to_noise_increments: dict[Function,list[int]],
+                     initial_condition: Function,
+                     time_to_det_forcing: dict[float,Function] | None = None,
+                     Reynolds_number: float = 1) -> tuple[dict[float,Function], dict[float,Function], dict[float,Function], dict[float,Function]]:
+    """Solve Stokes system with modified Chorin splitting. 
+    
+    Return 'time -> velocity', 'time -> total pressure', 'time -> stochastic pressure', and 'time -> deterministic pressure' dictionaries. """
+
+    Re = Constant(Reynolds_number)
+    tau = Constant(1.0)
+    noise_coeff_to_dW = {noise_coeff: Constant(1.0) for noise_coeff in noise_coeff_to_noise_increments}
+    det_forcing = Function(space_disc.velocity_space)
+
+    u = TrialFunction(space_disc.velocity_space)
+    v = TestFunction(space_disc.velocity_space)
+    uold = Function(space_disc.velocity_space)
+    unew = Function(space_disc.velocity_space)
+    utilde = Function(space_disc.velocity_space)
+    noise_projected = Function(space_disc.velocity_space)
+
+    p = TrialFunction(space_disc.pressure_space)
+    q = TestFunction(space_disc.pressure_space)
+    pnew = Function(space_disc.pressure_space)
+    psto = Function(space_disc.pressure_space)
+    pdet = Function(space_disc.pressure_space)
+
+    #dummy zero velocity field
+    fzero = Function(space_disc.velocity_space)
+
+    #variational form: stochastich pressure
+    a0 = inner(grad(p),grad(q))*dx
+    L0 = 1/tau*inner(fzero,grad(q))*dx
+    for noise_coeff in noise_coeff_to_noise_increments:
+        L0 = L0 + 1/tau*noise_coeff_to_dW[noise_coeff]*inner(noise_coeff, grad(q))*dx
+
+    #variational form: Helmholtz-projected noise
+    a1 = inner(u,v)*dx
+    L1 = -inner(grad(psto),v)*dx
+    for noise_coeff in noise_coeff_to_noise_increments:
+        L1 = L1 + 1/tau*noise_coeff_to_dW[noise_coeff]*inner(noise_coeff, v)*dx
+
+    #variational form: artificial velocity
+    a2 = ( inner(u,v) + 1/Re*tau*inner(grad(u), grad(v)) )*dx
+    L2 = ( inner(uold,v) + tau*inner(det_forcing,v) + tau*inner(noise_projected,v))*dx
+
+    #variational form: deterministic pressure
+    a3 = inner(grad(p),grad(q))*dx
+    L3 = 1/tau*inner(utilde,grad(q))*dx
+
+    #variational form: velocity
+    a4 = inner(u,v)*dx
+    L4 = ( inner(utilde,v) - tau*inner(grad(pdet),v) )*dx
+
+    V_basis = VectorSpaceBasis(constant=True)
+
+    uold.assign(initial_condition)
+
+    initial_time, time_increments = trajectory_to_incremets(time_grid)
+    time = initial_time
+
+ 
+    time_to_velocity = dict()
+    time_to_pressure = dict()
+    time_to_stochastic_pressure = dict()
+    time_to_deterministic_pressure = dict()
+
+    time_to_velocity[time] = deepcopy(uold)
+    time_to_pressure[time] = deepcopy(pnew)
+    time_to_stochastic_pressure[time] = deepcopy(pnew)
+    time_to_deterministic_pressure[time] = deepcopy(pnew)
+
+    for index in tqdm(range(len(time_increments))):
+        for noise_coeff in noise_coeff_to_noise_increments:
+            noise_coeff_to_dW[noise_coeff].assign(noise_coeff_to_noise_increments[noise_coeff][index])
+        tau.assign(time_increments[index])
+        time += time_increments[index]
+        if time_to_det_forcing:
+            try:
+                det_forcing.assign(time_to_det_forcing[time])
+            except KeyError as k:
+                print(f"Deterministic forcing couldn't be set.\nRequested time:\t {time}\nAvailable times:\t {list(time_to_det_forcing.keys())}")
+                raise k
+            
+        #Solve variational forms
+        solve(a0 == L0, psto, nullspace = V_basis)
+        solve(a1 == L1, noise_projected)
+        solve(a2 == L2, utilde, bcs=space_disc.bcs_vel)
+        solve(a3 == L3, pdet, nullspace = V_basis)
+        solve(a4 == L4, unew)
+
+        #Mean correction
+        mean_psto = Constant(assemble( inner(psto,1)*dx ))
+        psto.dat.data[:] = psto.dat.data - Function(space_disc.pressure_space).assign(mean_psto).dat.data
+        mean_pdet = Constant(assemble( inner(pdet,1)*dx ))
+        pdet.dat.data[:] = pdet.dat.data - Function(space_disc.pressure_space).assign(mean_pdet).dat.data
+
+        #Compute total pressure
+        pnew.dat.data[:] = pdet.dat.data + psto.dat.data
+
+        time_to_velocity[time] = deepcopy(unew)
+        time_to_pressure[time] = deepcopy(pnew)
+        time_to_stochastic_pressure[time] = deepcopy(psto)
+        time_to_deterministic_pressure[time] = deepcopy(pdet)
+
+        uold.assign(unew)
+
+    return time_to_velocity, time_to_pressure, time_to_stochastic_pressure, time_to_deterministic_pressure
 
 def impliciteEuler_mixedFEM_ito_transportNoise(space_disc: SpaceDiscretisation,
                            time_grid: list[float],
