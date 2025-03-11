2e0a72232378d055c405f63fad593cdaad7258b9
Joern Wichmann <joern.wichmann@monash.edu>
2025-03-11 17:36:32 +1100

    added a new transport field that presumably gives worse rates with a test

--- src/noise_coefficients.py
+++ src/noise_coefficients.py
@@ -13,7 +13,7 @@ def NON_SOLENOIDAL_CarelliHausenblasProhl(end_level: int, mesh: MeshGeometry, ve
         for k in range(1,end_level+1):
             expr = 1/((j+k)*(j+k))*as_vector([
                 sin(j*pi*x)*sin(k*pi*y),
-                sin(k*pi*x)*sin(j*pi*y)
+                sin(j*pi*x)*sin(k*pi*y)
                 ])
             noiseCoefficients.append(project(expr, velocity_space))
     return noiseCoefficients
@@ -33,4 +33,22 @@ def SOLENOIDAL_CarelliHausenblasProhl(end_level: int, mesh: MeshGeometry, veloci
             sin(j*pi*x - pi/2.0)*cos(j*pi*y - pi/2.0)
             ])
         noiseCoefficients.append(project(expr, velocity_space))
+    return noiseCoefficients
+
+def NON_SOLENOIDAL_NEW(end_level: int, mesh: MeshGeometry, velocity_space: FunctionSpace) -> list[Function]:
+    """Returns a list of noise coefficients based on the construction for NON-SOLENOIDAL, additive noise in Carelli, Hausenblas and Prohl.
+    Parameters: 
+        -   end_level:  truncation index of the sum
+        -   mesh:       triangulation of the domain
+        -   velocity_space: finite element space used for the approximation of velocity
+    """
+    x, y = SpatialCoordinate(mesh)
+    noiseCoefficients = []
+    for j in range(1,end_level+1):
+        for k in range(1,end_level+1):
+            expr = 1/((j+k)*(j+k))*as_vector([
+                sin(j*pi*x)*sin(k*pi*y),
+                sin(k*pi*x)*sin(j*pi*y)
+                ])
+            noiseCoefficients.append(project(expr, velocity_space))
     return noiseCoefficients
